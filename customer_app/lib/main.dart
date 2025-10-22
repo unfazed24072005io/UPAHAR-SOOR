9cea4bcb1e5d2c44fb4131c78556a70b3e397b8f
SUTIRTHA SOOR <sutirthasoor7@gmail.com>
2025-10-22 15:02:32 +0530

    Update main.dart

--- customer_app/lib/main.dart
+++ customer_app/lib/main.dart
@@ -76,8 +76,9 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
+      home: SplashScreen(),
     );
   }
 }
+
 
