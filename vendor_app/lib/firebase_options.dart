dc503d7e75f2543c24448e942c790b19ac9b6252
SUTIRTHA SOOR <sutirthasoor7@gmail.com>
2025-10-22 17:05:43 +0530

    Update firebase_options.dart

--- vendor_app/lib/firebase_options.dart
+++ vendor_app/lib/firebase_options.dart
@@ -10,13 +10,13 @@ class DefaultFirebaseOptions {
       );
     }
 
-    // ANDROID CONFIGURATION FOR VENDOR APP
+    // CORRECT ANDROID CONFIGURATION FOR VENDOR APP
     const android = FirebaseOptions(
       apiKey: 'AIzaSyAyeLCH83Xj2A2w28N2A4_4HWaePCuLTAg',
       appId: '1:531294896795:android:ada2d28fe89b31942afc3e',
       messagingSenderId: '531294896795',
-      projectId: 'upahar-ecommerce', // Your project ID
-      storageBucket: 'upahar-ecommerce.appspot.com',
+      projectId: 'upahar-bacb3', // ← CORRECT PROJECT ID
+      storageBucket: 'upahar-bacb3.firebasestorage.app', // ← CORRECT BUCKET
     );
 
     return android;
